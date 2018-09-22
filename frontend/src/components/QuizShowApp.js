14d3c640bcd13f427041ebd7ec6888f2dabdf19f
Clinton Dreisbach <clinton@dreisbach.us>
2018-09-22 10:47:38 -0400

    Add initial dependencies

---
+++ frontend/src/components/QuizShowApp.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class QuizShowApp extends Component {
+  render () {
+    return (
+      <div className='QuizShowApp'>
+        <header className='QuizShowApp-header'>
+          <h1 className='QuizShowApp-title'>Welcome to React</h1>
+        </header>
+        <p className='QuizShowApp-intro'>
+          To get started, edit <code>src/QuizShowApp.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default QuizShowApp
