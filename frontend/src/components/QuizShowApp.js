fd81c3eda024bca5cfd6ddb3582e824b946f598a
Clinton Dreisbach <clinton@dreisbach.us>
2018-09-26 09:46:20 -0400

    Use localforage for user storage

--- frontend/src/components/QuizShowApp.js
+++ frontend/src/components/QuizShowApp.js
@@ -3,37 +3,48 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import data from '../data'
 import Dashboard from './Dashboard'
+import FetchQuiz from './FetchQuiz'
 import FetchQuizzes from './FetchQuizzes'
 import Guard from './Guard'
 import Login from './Login'
 import Navigation from './Navigation'
-import Register from './Register'
 import Quiz from './Quiz'
-import FetchQuiz from './FetchQuiz'
+import Register from './Register'
+import localforage from 'localforage'
 
 class QuizShowApp extends Component {
   constructor () {
     super()
     this.state = {
-      currentUser: null
-    }
-
-    const email = window.localStorage.getItem('email')
-    const token = window.localStorage.getItem('token')
-    if (email && token) {
-      this.state.currentUser = { email, token }
-      data.setUserToken(token)
+      currentUser: null,
+      userLoaded: false
     }
 
     this.setCurrentUser = this.setCurrentUser.bind(this)
   }
 
+  componentDidMount () {
+    localforage.getItem('user')
+      .then(user => {
+        if (user && user.token) {
+          data.setUserToken(user.token)
+          this.setState({
+            userLoaded: true,
+            currentUser: user
+          })
+        } else {
+          this.setState({
+            userLoaded: true,
+            currentUser: null
+          })
+        }
+      })
+  }
+
   setCurrentUser (user) {
-    window.localStorage.setItem('email', user.email)
-    window.localStorage.setItem('token', user.token)
-    this.setState({
-      currentUser: user
-    })
+    localforage.setItem('user', user)
+      .then(() => localforage.getItem('user'))
+      .then(user => this.setState({ currentUser: user }))
   }
 
   logout () {
@@ -43,7 +54,7 @@ class QuizShowApp extends Component {
   }
 
   render () {
-    const { currentUser } = this.state
+    const { currentUser, userLoaded } = this.state
     return (
       <Router>
         <div className='QuizShowApp'>
@@ -54,18 +65,18 @@ class QuizShowApp extends Component {
                 <FetchQuizzes render={({ quizzes, loading }) =>
                   <Dashboard loggedIn={!!currentUser} loading={loading} quizzes={quizzes} />} />} />
               <Route path='/quizzes/:id' render={({ match }) =>
-                <Guard condition={currentUser} redirectTo='/login'>
+                <Guard condition={!userLoaded || currentUser} redirectTo='/login'>
                   <FetchQuiz
                     id={match.params.id}
                     render={({ quiz, loading }) => <Quiz loading={loading} quiz={quiz} />} />
                 </Guard>} />
               <Route path='/login' render={() =>
-                <Guard condition={!currentUser} redirectTo='/'>
+                <Guard condition={!userLoaded || !currentUser} redirectTo='/'>
                   <Login setCurrentUser={this.setCurrentUser} />
                 </Guard>
               } />
               <Route path='/register' render={() =>
-                <Guard condition={!currentUser} redirectTo='/'>
+                <Guard condition={!userLoaded || !currentUser} redirectTo='/'>
                   <Register setCurrentUser={this.setCurrentUser} />
                 </Guard>
               } />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,15 @@
+import 'bulma/css/bulma.css'
+import localforage from 'localforage'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'bulma/css/bulma.css'
-import './index.css'
 import App from './components/QuizShowApp'
+import './index.css'
 import registerServiceWorker from './registerServiceWorker'
+
+localforage.config({
+  name: 'QuizShow',
+  version: 1.0
+})
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
