c14f2cdcba98e77cf7efab63a26208ce3f1ddc21
Clinton Dreisbach <clinton@dreisbach.us>
2018-09-25 20:48:25 -0400

    Show previous score

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -9,22 +9,32 @@ class Dashboard extends React.Component {
     return (
       <div className='Dashboard'>
         <Subtitle>Available quizzes</Subtitle>
-        <ul>
-          {quizzes.map(quiz => <li key={quiz.id}><QuizLink loggedIn={loggedIn} quiz={quiz} /></li>)}
-        </ul>
+        {quizzes.map(quiz => <div key={quiz.id}><QuizLink loggedIn={loggedIn} quiz={quiz} /></div>)}
       </div>
     )
   }
 }
 
 const QuizLink = ({ loggedIn, quiz }) => {
-  if (loggedIn) {
-    return (
-      <Link to={`/quizzes/${quiz.id}`}>{quiz.title}</Link>
-    )
-  } else {
-    return <React.Fragment>{quiz.title}</React.Fragment>
-  }
+  return (
+    <div className='QuizLink'>
+      {loggedIn
+        ? <React.Fragment>
+          <Link to={`/quizzes/${quiz.id}`}>
+            <span className='is-size-5'>{quiz.title}</span>
+          </Link>
+          <br />
+          {quiz.prevScore
+            ? <span>Previous score: {percent(quiz.prevScore)}</span>
+            : <span>Not previously taken</span>}
+        </React.Fragment>
+        : <span className='is-size-5'>{quiz.title}</span>}
+    </div>
+  )
+}
+
+const percent = (n) => {
+  return `${Math.round(n * 10 ** 4) / 10 ** 2}%`
 }
 
 Dashboard.propTypes = {
